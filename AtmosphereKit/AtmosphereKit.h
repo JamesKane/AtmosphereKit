1b74bc6c6c6aeece851ef381761fd90e41962b2e
James R Kane <jkane@tds.net>
2025-03-19 04:48:16 -0500

    Initial Commit

---
+++ AtmosphereKit/AtmosphereKit.h
@@ -0,0 +1,18 @@
+//
+//  AtmosphereKit.h
+//  AtmosphereKit
+//
+//  Created by James Kane on 3/19/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AtmosphereKit.
+FOUNDATION_EXPORT double AtmosphereKitVersionNumber;
+
+//! Project version string for AtmosphereKit.
+FOUNDATION_EXPORT const unsigned char AtmosphereKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AtmosphereKit/PublicHeader.h>
+
+
---
+++ AtmosphereKitTests/AtmosphereKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  AtmosphereKitTests.swift
+//  AtmosphereKitTests
+//
+//  Created by James Kane on 3/19/25.
+//
+
+import Testing
+@testable import AtmosphereKit
+
+struct AtmosphereKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
